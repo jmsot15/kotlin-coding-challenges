4810397925a86e85ebe3c38ac83ceb022a002600
Jerikka <jesotelo@proton.me>
2025-07-06 17:22:35 -0400

    Solved range2 in range1

--- src/test/kotlin/com/igorwojda/range/containsrange/Challenge.kt
+++ src/test/kotlin/com/igorwojda/range/containsrange/Challenge.kt
@@ -1,10 +1,11 @@
 package com.igorwojda.range.containsrange
 
+import org.amshove.kluent.`should contain same`
 import org.amshove.kluent.shouldBeEqualTo
 import org.junit.jupiter.api.Test
 
 private fun containsRange(range1: IntRange, range2: IntRange): Boolean {
-    TODO("Add your solution here")
+    return range1.first <= range2.first && range1.last >= range2.last
 }
 
 private class Test {
