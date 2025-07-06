265fe9c5b99e05b1776a3d0b28cca634254845fb
Jerikka <jesotelo@proton.me>
2025-07-06 17:27:56 -0400

    Optimized addupto

--- src/test/kotlin/com/igorwojda/integer/addupto/Challenge.kt
+++ src/test/kotlin/com/igorwojda/integer/addupto/Challenge.kt
@@ -4,12 +4,7 @@ import org.amshove.kluent.shouldBeEqualTo
 import org.junit.jupiter.api.Test
 
 private fun addUpTo(n: Int): Int {
-    var total = 0
-    for (i in 1..n) {
-        total += i
-    }
-
-    return total
+    return (1..n).sum()
 }
 
 private class Test {
